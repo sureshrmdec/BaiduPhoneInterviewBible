c3f6c6f22baea989c79947aaaed7b659d930772c
Jerome <jjlzyz000@gmail.com>
2016-06-24 14:53:46 -0400

    add xcode project

---
+++ BaiduPhoneInterview/BaiduPhoneInterview/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  BaiduPhoneInterview
+//
+//  Created by Jerome on 6/24/16.
+//  Copyright © 2016 Jerome. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
